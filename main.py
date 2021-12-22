230fe0870f2960d20f2d5ab67e15644bcac7cf65
anime fanatic <beol0127@gmail.com>
2021-12-22 16:22:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, url_for, redirect, sessions
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route('/home')
+def home():
+  return render_template("home.html")
+
+@app.route('/create', methods = ["GET", "POST"])
+def create():
+  if request.method == "GET":
+    return render_template("create.html")
+  if request.method == "POST":
+    list_name = request.form["listname"]
+    contents = request.form["contents"]
+
+    return redirect(url_for('home'))
+
+app.run(host = "0.0.0.0", port = 3000)
